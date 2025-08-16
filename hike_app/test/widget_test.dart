577aa39687442c2c3b903c7e722515dc4fa30ce1
sarveshsalokhe26 <sarveshsalokhe26@gmail.com>
2025-08-17 04:09:17 +0530

    Obscure

--- hike_app/test/widget_test.dart
+++ hike_app/test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:hike_app/main.dart';
+import 'package:hike_app/Screens/signup_screen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
